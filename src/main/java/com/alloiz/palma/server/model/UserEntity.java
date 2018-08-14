6df25ab32d80e0910ed5d3b706017e8cfc978efe
User <xandriy58@gmail.com>
2018-08-14 15:11:27 +0300

    created UserEntity

--- src/main/java/com/alloiz/palma/server/model/UserEntity.java
+++ src/main/java/com/alloiz/palma/server/model/UserEntity.java
@@ -1,4 +1,78 @@
 package com.alloiz.palma.server.model;
 
-public class UserEntity {
+import com.alloiz.palma.server.model.enums.Role;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Entity
+public class UserEntity extends BaseEntity<UserEntity>{
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public UserEntity() { }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserEntity setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserEntity setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public UserEntity setLogin(String login) {
+        this.login = login;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public UserEntity setRole(Role role) {
+        this.role = role;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
+
+
---
+++ src/main/java/com/alloiz/palma/server/model/enums/Role.java
@@ -0,0 +1,5 @@
+package com.alloiz.palma.server.model.enums;
+
+public enum Role {
+    ADMIN, MODERATOR
+}

a9234cea7bbe252c631dc84d45ba049c12eb35d7
User <xandriy58@gmail.com>
2018-08-14 12:48:56 +0300

    Addded main structure

--- src/main/java/com/alloiz/palma/server/ServerApplication.java
+++ src/main/java/com/alloiz/palma/server/ServerApplication.java
@@ -2,11 +2,20 @@ package com.alloiz.palma.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class ServerApplication {
+@EnableJpaRepositories(basePackages = "com.alloiz.palma.server.repository")
+public class ServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServerApplication.class);
     }
 }

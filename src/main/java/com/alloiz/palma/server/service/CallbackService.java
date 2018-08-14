68d49b41a660e7e089328ae95cc9e2d008500d36
unknown <bohdanrud5180@gmail.com>
2018-08-14 17:54:27 +0300

    added Services

--- src/main/java/com/alloiz/palma/server/service/CallbackService.java
+++ src/main/java/com/alloiz/palma/server/service/CallbackService.java
@@ -1,4 +1,22 @@
 package com.alloiz.palma.server.service;
 
+import com.alloiz.palma.server.model.Callback;
+
+import java.util.List;
+
 public interface CallbackService {
+
+    Callback findOneAvailable(Long id);
+
+    List<Callback> findAllAvailable();
+
+    Callback findOne(Long id);
+
+    List<Callback> findAll();
+
+    Callback save(Callback callback);
+
+    Callback update(Callback update);
+
+    Boolean delete(Long id);
 }

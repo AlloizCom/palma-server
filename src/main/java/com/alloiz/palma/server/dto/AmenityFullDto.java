a1743f1347cca65148415e24988ed06cc3d08f06
unknown <bohdanrud5180@gmail.com>
2018-08-15 11:58:20 +0300

    added AmenityDto

--- src/main/java/com/alloiz/palma/server/dto/AmenityFullDto.java
+++ src/main/java/com/alloiz/palma/server/dto/AmenityFullDto.java
@@ -1,4 +1,46 @@
 package com.alloiz.palma.server.dto;
 
-public class AmenityFullDto {
+import com.alloiz.palma.server.dto.utils.annotations.Dto;
+import com.alloiz.palma.server.model.AmenityName;
+import com.alloiz.palma.server.model.Room;
+
+import java.util.List;
+
+@Dto
+public class AmenityFullDto extends AmenityShortDto<AmenityFullDto>{
+
+    protected Room room;
+    protected List<AmenityName> amenityNames;
+
+    public AmenityFullDto() {
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public AmenityFullDto setRoom(Room room) {
+        this.room = room;
+        return this;
+    }
+
+    public List<AmenityName> getAmenityNames() {
+        return amenityNames;
+    }
+
+    public AmenityFullDto setAmenityNames(List<AmenityName> amenityNames) {
+        this.amenityNames = amenityNames;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "AmenityFullDto{" +
+                "room=" + room +
+                ", amenityNames=" + amenityNames +
+                ", id=" + id +
+                ", imagePath='" + imagePath + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }
--- src/main/java/com/alloiz/palma/server/dto/AmenityShortDto.java
+++ src/main/java/com/alloiz/palma/server/dto/AmenityShortDto.java
@@ -1,4 +1,50 @@
 package com.alloiz.palma.server.dto;
 
-public class AmenityShortDto {
+import com.alloiz.palma.server.dto.utils.annotations.Dto;
+
+@Dto
+public class AmenityShortDto <T extends AmenityShortDto>{
+
+    protected Long id;
+    protected String imagePath;
+    protected Boolean available;
+
+    public AmenityShortDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public T setId(Long id) {
+        this.id = id;
+        return (T)this;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public T setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+        return (T)this;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public T setAvailable(Boolean available) {
+        this.available = available;
+        return (T) this;
+    }
+
+    @Override
+    public String toString() {
+        return "AmenityShortDto{" +
+                "id=" + id +
+                ", imagePath='" + imagePath + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }

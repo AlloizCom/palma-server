c49b8f744c5c910d53b02bde567a0c6694123a80
unknown <bohdanrud5180@gmail.com>
2018-08-15 17:40:26 +0300

    added book controller

--- src/main/java/com/alloiz/palma/server/controller/BookController.java
+++ src/main/java/com/alloiz/palma/server/controller/BookController.java
@@ -1,4 +1,61 @@
 package com.alloiz.palma.server.controller;
 
+import com.alloiz.palma.server.dto.BookDto;
+import com.alloiz.palma.server.model.Book;
+import com.alloiz.palma.server.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.alloiz.palma.server.dto.utils.builder.Builder.map;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/find-all")
+    private ResponseEntity<List<BookDto>> findAll() {
+        return ResponseEntity.ok(bookService.findAll().stream()
+                .map(callback -> map(callback, BookDto.class)).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/find-all-available")
+    private ResponseEntity<List<BookDto>> findAllAvailable() {
+        return ResponseEntity.ok(bookService.findAllAvailable().stream()
+                .map(callback -> map(callback, BookDto.class)).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/find-one-available/{id}")
+    private ResponseEntity<BookDto> findOneAvailale(@PathVariable Long id) {
+        return ResponseEntity.ok(map(bookService.findOneAvailable(id), BookDto.class));
+    }
+
+    @GetMapping("/find-one/{id}")
+    private ResponseEntity<BookDto> findOne(@PathVariable Long id) {
+        return ResponseEntity.ok(map(bookService.findOne(id), BookDto.class));
+    }
+
+    @PostMapping("/save")
+    private ResponseEntity<BookDto> save(@RequestBody BookDto book) {
+        return ResponseEntity
+                .ok(map(bookService.save(map(book, Book.class)), BookDto.class));
+    }
+
+    @PostMapping("/update")
+    private ResponseEntity<BookDto> update(@RequestBody BookDto book) {
+        return ResponseEntity
+                .ok(map(bookService.update(map(book, Book.class)), BookDto.class));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    private ResponseEntity<Boolean> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.delete(id));
+    }
+
 }

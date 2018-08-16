dd6369759ba2907ca8a8751252d44de34d94a628
unknown <bohdanrud5180@gmail.com>
2018-08-16 16:14:51 +0300

    added Image Repository

--- src/main/java/com/alloiz/palma/server/model/Image.java
+++ src/main/java/com/alloiz/palma/server/model/Image.java
@@ -1,4 +1,30 @@
 package com.alloiz.palma.server.model;
 
-public class Image {
+import javax.persistence.Entity;
+
+@Entity
+public class Image extends BaseEntity<Image>{
+
+    private String path;
+
+    public Image() {
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Image setPath(String path) {
+        this.path = path;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "path='" + path + '\'' +
+                ", id=" + id +
+                ", available=" + available +
+                '}';
+    }
 }

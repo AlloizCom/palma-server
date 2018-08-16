e976cda5a6066e296056cfb22195586ebe4f0855
unknown <bohdanrud5180@gmail.com>
2018-08-16 17:44:46 +0300

    added delete methods for Room controller

--- src/main/java/com/alloiz/palma/server/controller/RoomController.java
+++ src/main/java/com/alloiz/palma/server/controller/RoomController.java
@@ -61,6 +61,16 @@ public class RoomController {
         //}
     }
 
+    @PostMapping("/add-image")
+    private ResponseEntity<RoomFullDto> addImage(@RequestParam MultipartFile[] multipartFiles, @PathVariable Long id) {
+        return ResponseEntity.ok(map(roomService.addImage(multipartFiles, id), RoomFullDto.class));
+    }
+
+    @PostMapping("/delete-image")
+    private ResponseEntity<RoomFullDto> deleteImage(@PathVariable Long roomId, Long imageId) {
+        return ResponseEntity.ok(map(roomService.deleteImage(roomId, imageId), RoomFullDto.class));
+    }
+
     @DeleteMapping("/delete/{id}")
     private ResponseEntity<Boolean> delete(@PathVariable Long id) {
         return ResponseEntity.ok(roomService.delete(id));

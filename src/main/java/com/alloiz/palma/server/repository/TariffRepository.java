73904e68354ad2796d03cac3e9d443e375375389
unknown <bohdanrud5180@gmail.com>
2018-08-17 11:03:36 +0300

    added repository for Tariff

--- src/main/java/com/alloiz/palma/server/repository/TariffRepository.java
+++ src/main/java/com/alloiz/palma/server/repository/TariffRepository.java
@@ -2,9 +2,11 @@ package com.alloiz.palma.server.repository;
 
 import com.alloiz.palma.server.model.Tariff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TariffRepository extends JpaRepository<Tariff,Long> {
 
     List<Tariff> findAllByAvailable(Boolean available);

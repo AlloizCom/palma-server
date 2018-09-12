a30bbc88a3d5bb4fff8c2ba9c43ff8aa08cf407b
unknown <bohdanrud5180@gmail.com>
2018-09-12 13:53:05 +0300

    added methods in to the Schedule Controller

--- src/main/java/com/alloiz/palma/server/controller/ScheduleController.java
+++ src/main/java/com/alloiz/palma/server/controller/ScheduleController.java
@@ -1,8 +1,17 @@
 package com.alloiz.palma.server.controller;
 
+import com.alloiz.palma.server.dto.ScheduleDto;
+import com.alloiz.palma.server.model.Schedule;
+import com.alloiz.palma.server.service.ScheduleService;
 import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.alloiz.palma.server.dto.utils.builder.Builder.map;
 
 /**
  * Controller for Schedule
@@ -12,4 +21,63 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleController {
 
     private static final Logger LOGGER = Logger.getLogger(ScheduleController.class);
+
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @GetMapping("/find-all")
+    private ResponseEntity<List<ScheduleDto>> findAll() {
+        return ResponseEntity.ok(scheduleService.findAll().stream()
+                .map(callback -> map(callback, ScheduleDto.class))
+                .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/find-all-available")
+    private ResponseEntity<List<ScheduleDto>> findAllAvailable() {
+        return ResponseEntity.ok(scheduleService.findAllAvailable().stream()
+                .map(callback -> map(callback, ScheduleDto.class))
+                .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/find-one/{id}")
+    private ResponseEntity<ScheduleDto> findOne(@PathVariable Long id) {
+        return ResponseEntity
+                .ok(map(scheduleService.findOne(id), ScheduleDto.class));
+    }
+
+    @GetMapping("/find-one-available/{id}")
+    private ResponseEntity<ScheduleDto> findOneAvailale(@PathVariable Long id) {
+        return ResponseEntity
+                .ok(map(scheduleService.findOneAvailable(id), ScheduleDto.class));
+    }
+
+    @PostMapping("/save")
+    private ResponseEntity<ScheduleDto> save(@RequestBody ScheduleDto schedule) {
+        LOGGER.info(">>> Schedule Controller");
+        LOGGER.info("--------------------Schedule Save--------------------");
+        LOGGER.info(schedule);
+        LOGGER.info("--------------------Schedule Save--------------------");
+        return ResponseEntity
+                .ok(map(scheduleService.save(map(schedule, Schedule.class)), ScheduleDto.class));
+    }
+
+    @PostMapping("/update")
+    private ResponseEntity<ScheduleDto> update(@RequestBody ScheduleDto schedule) {
+        LOGGER.info(">>> Schedule Controller");
+        LOGGER.info("--------------------Schedule Update--------------------");
+        LOGGER.info(schedule);
+        LOGGER.info("--------------------Schedule Update--------------------");
+        return ResponseEntity
+                .ok(map(scheduleService.update(map(schedule, Schedule.class)), ScheduleDto.class));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    private ResponseEntity<Boolean> delete(@PathVariable Long id) {
+        LOGGER.info(">>> Schedule Controller");
+        LOGGER.info("--------------------Schedule Delete--------------------");
+        LOGGER.info("Schedule id: " + id);
+        LOGGER.info("--------------------Schedule Delete--------------------");
+        return ResponseEntity.ok(scheduleService.delete(id));
+    }
+
 }

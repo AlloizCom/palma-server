8886f73fa38235f4b91213260809fc6e9c53ffeb
Andriy Khalus <xandriy58@gmail.com>
2018-09-12 18:23:05 +0300

    added auto adding data to schedule

--- src/main/java/com/alloiz/palma/server/ServerApplication.java
+++ src/main/java/com/alloiz/palma/server/ServerApplication.java
@@ -1,10 +1,15 @@
 package com.alloiz.palma.server;
 
+import com.alloiz.palma.server.model.Schedule;
+import com.alloiz.palma.server.model.enums.RoomType;
+import com.alloiz.palma.server.repository.ScheduleRepository;
+import com.alloiz.palma.server.service.ScheduleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -12,7 +17,24 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+
+        ConfigurableApplicationContext context =
+                SpringApplication.run(ServerApplication.class, args);
+        generateSchedule(context);
+    }
+
+    private static void generateSchedule(ConfigurableApplicationContext context){
+        ScheduleService scheduleRepository = context.getBean(ScheduleService.class);
+
+        if(scheduleRepository.findAll().size() < 100){
+            for (int i = 0;i < 20;i++) {
+                scheduleRepository.save(new Schedule().setForSale(6).setActive(0).setFree(6).setRoomType(RoomType.DELUXE));
+                scheduleRepository.save(new Schedule().setForSale(6).setActive(0).setFree(6).setRoomType(RoomType.STANDARD));
+                scheduleRepository.save(new Schedule().setForSale(6).setActive(0).setFree(6).setRoomType(RoomType.STANDARD_IMPROVED));
+                scheduleRepository.save(new Schedule().setForSale(6).setActive(0).setFree(6).setRoomType(RoomType.SUPERIOR));
+                scheduleRepository.save(new Schedule().setForSale(6).setActive(0).setFree(6).setRoomType(RoomType.SUPERIOR_IMPROVED));
+            }
+        }
     }
 
     @Override

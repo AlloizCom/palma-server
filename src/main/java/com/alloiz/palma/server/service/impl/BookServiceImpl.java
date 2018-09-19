da39a7aef55f4a0e659e1f348e9131461685c29c
unknown <bohdanrud5180@gmail.com>
2018-09-19 11:41:39 +0300

    added pageable in to the BookServiceImpl

--- src/main/java/com/alloiz/palma/server/service/impl/BookServiceImpl.java
+++ src/main/java/com/alloiz/palma/server/service/impl/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.alloiz.palma.server.service.impl;
 
+import com.alloiz.palma.server.dto.BookByPage;
+import com.alloiz.palma.server.dto.BookDto;
 import com.alloiz.palma.server.model.Book;
 import com.alloiz.palma.server.model.Room;
 import com.alloiz.palma.server.model.Schedule;
@@ -11,19 +13,24 @@ import com.alloiz.palma.server.service.RoomService;
 import com.alloiz.palma.server.service.ScheduleService;
 import com.alloiz.palma.server.service.exceptions.NotEnoughFreePlacesException;
 import com.alloiz.palma.server.service.exceptions.RoomTypeNotFoundException;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.alloiz.palma.server.dto.utils.builder.Builder.map;
 import static com.alloiz.palma.server.service.utils.GenerateUuid.generateUuid;
 import static com.alloiz.palma.server.service.utils.Validation.*;
 import static java.util.stream.Collectors.toList;
 
 @Service
 public class BookServiceImpl implements BookService {
+
+    private static final Logger LOGGER = Logger.getLogger(BookServiceImpl.class);
 
     @Autowired
     private BookRepository bookRepository;
@@ -143,5 +150,41 @@ public class BookServiceImpl implements BookService {
     public Book findByUuid(String uuid) {
         checkString(uuid);
         return bookRepository.findByUuid(uuid);
+    }
+
+    /**
+     *
+     * @param pageable
+     * @return
+     */
+    @Override
+    public List<Book> findAll(Pageable pageable) {
+        List<Book> schedules = bookRepository.findAll(pageable).getContent();
+        return schedules;
+    }
+
+    /**
+     *
+     * @param pageable
+     * @return
+     */
+    @Override
+    public BookByPage findAllByAvailable(Pageable pageable) {
+        LOGGER.info(">>> Page number:" + pageable.getPageNumber());
+        LOGGER.info(">>> Page size:" + pageable.getPageSize());
+        List<BookDto> bookList = bookRepository
+                .findAllByAvailable(true, pageable)
+                .getContent()
+                .stream()
+                .map(book -> map(book, BookDto.class))
+                .collect(toList());
+        LOGGER.info("-------------Book Page---------------");
+        bookList.stream().forEach(bookDto -> LOGGER.info(bookDto.getId()));
+        return new BookByPage()
+                .setBookDtoList(bookList)
+                .setCurrentPage(pageable.getPageNumber())
+                .setNumberOfItems(pageable.getPageSize())
+                .setNumberOfPages((bookRepository
+                        .countAllByAvailable(true) / pageable.getPageSize()) + 1);
     }
 }

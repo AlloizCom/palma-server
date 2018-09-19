fb0ca1eec796966930e0a495966d8bac830714a0
unknown <bohdanrud5180@gmail.com>
2018-09-19 14:03:40 +0300

    added default data creator

---
+++ src/main/java/com/alloiz/palma/server/config/DefaultDataCreator.java
@@ -0,0 +1,64 @@
+package com.alloiz.palma.server.config;
+
+import com.alloiz.palma.server.service.BookCounterService;
+import com.alloiz.palma.server.service.TariffService;
+import com.alloiz.palma.server.service.UserEntityService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Default data creator
+ */
+@Component
+public class DefaultDataCreator {
+
+    private static final Logger LOGGER
+            = Logger.getLogger(DefaultDataCreator.class);
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private UserEntityService userEntityService;
+
+    @Autowired
+    private BookCounterService bookCounterService;
+
+    @Autowired
+    private TariffService tariffService;
+
+    /**
+     * This method starts once after server was run,
+     * and creates default data in the data base:
+     * User, Moderator, Tariffs, Counter
+     */
+    @PostConstruct
+    public void init(){
+        LOGGER.warn("---Creating Admin/Moderator---");
+        if (userEntityService.createDefaultUser()){
+            LOGGER.warn(">>>---Admin/Moderator was created---");
+        } else {
+            LOGGER.warn(">>>---Admin/Moderator already exist---");
+        }
+        LOGGER.warn("----------------------------------");
+        LOGGER.warn("---Creating Counter for Booking---");
+        if (bookCounterService.createDefaultCounter()){
+            LOGGER.warn(">>>---Counter was created---");
+        } else {
+            LOGGER.warn(">>>---Counter already exists---");
+        }
+        LOGGER.warn("----------------------------------");
+        LOGGER.warn("---Creating default Tariff---");
+        LOGGER.warn("-----------------------------");
+        if (tariffService.createDefaultTariffs(true)){
+            LOGGER.warn(">>>---Default tariffs were created---");
+        } else {
+            LOGGER.warn(">>>---Default tariffs already exist---");
+        }
+        LOGGER.warn("----------------------------------");
+    }
+}

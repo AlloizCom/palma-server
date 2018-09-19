f78e3818f170d08e359543afbbac247002f9c4a0
Andriy Khalus <xandriy58@gmail.com>
2018-09-19 13:18:19 +0300

    added BookCounter

---
+++ src/main/java/com/alloiz/palma/server/model/BookCounter.java
@@ -0,0 +1,31 @@
+package com.alloiz.palma.server.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class BookCounter extends BaseEntity<BookCounter>{
+
+    private Long numberOfBooking;
+
+    public BookCounter() {
+        this.numberOfBooking = 0L;
+    }
+
+    public Long getNumberOfBooking() {
+        return numberOfBooking;
+    }
+
+    public BookCounter setNumberOfBooking(Long numberOfBooking) {
+        this.numberOfBooking = numberOfBooking;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "BookCounter{" +
+                "numberOfBooking=" + numberOfBooking +
+                ", id=" + id +
+                ", available=" + available +
+                '}';
+    }
+}

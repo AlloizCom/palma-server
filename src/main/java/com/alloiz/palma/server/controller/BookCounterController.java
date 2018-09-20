5a495f5c627b3d1a5738816ac6e2a0ff7cd0dd02
Andriy Khalus <xandriy58@gmail.com>
2018-09-20 12:44:59 +0300

    added template for websocket

--- src/main/java/com/alloiz/palma/server/controller/BookCounterController.java
+++ src/main/java/com/alloiz/palma/server/controller/BookCounterController.java
@@ -1,10 +1,27 @@
 package com.alloiz.palma.server.controller;
 
+import com.alloiz.palma.server.model.BookCounter;
+import com.alloiz.palma.server.service.BookCounterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController()
 public class BookCounterController {
 
+    @Autowired
+    private SimpMessagingTemplate template;
 
+    @Autowired
+    private BookCounterService bookCounterService;
+
+    @GetMapping("/notify")
+    public BookCounter getNotification(){
+        template.convertAndSend("/booking",bookCounterService.getCounter(1L));
+        return bookCounterService.getCounter(1L);
+    }
 }

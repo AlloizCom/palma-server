c264d1653b95c635f85bd3ea95f7e8d71f2e2d0d
Andriy Khalus <xandriy58@gmail.com>
2018-09-20 14:59:39 +0300

    added reset and increment counter to controller

--- src/main/java/com/alloiz/palma/server/controller/BookCounterController.java
+++ src/main/java/com/alloiz/palma/server/controller/BookCounterController.java
@@ -3,14 +3,12 @@ package com.alloiz.palma.server.controller;
 import com.alloiz.palma.server.model.BookCounter;
 import com.alloiz.palma.server.service.BookCounterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController("/counter")
 public class BookCounterController {
 
     @Autowired
@@ -20,8 +18,18 @@ public class BookCounterController {
     private BookCounterService bookCounterService;
 
     @GetMapping("/notify")
-    public BookCounter getNotification(){
-        template.convertAndSend("/booking",bookCounterService.getCounter(1L));
+    public BookCounter getNotification() {
+        template.convertAndSend("/booking", bookCounterService.getCounter(1L));
         return bookCounterService.getCounter(1L);
+    }
+
+    @GetMapping("/reset")
+    public ResponseEntity<BookCounter> resetCounter(){
+        return ResponseEntity.ok(bookCounterService.resetCounter(1L));
+    }
+
+    @GetMapping("/increment")
+    public ResponseEntity<BookCounter> incrementCounter(){
+        return ResponseEntity.ok(bookCounterService.incrementCounter(1L));
     }
 }

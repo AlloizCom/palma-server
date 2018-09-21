ab11bd4af7b4a041df603f3d49206daa374aa0d8
unknown <bohdanrud5180@gmail.com>
2018-09-21 10:42:17 +0300

    added pageable in to the Callback repository

--- src/main/java/com/alloiz/palma/server/repository/CallbackRepository.java
+++ src/main/java/com/alloiz/palma/server/repository/CallbackRepository.java
@@ -1,6 +1,8 @@
 package com.alloiz.palma.server.repository;
 
 import com.alloiz.palma.server.model.Callback;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,10 @@ public interface CallbackRepository extends JpaRepository<Callback, Long> {
     List<Callback> findAllByAvailable(Boolean available);
 
     Callback findByAvailableAndId(Boolean available, Long id);
+
+    Page<Callback> findAllByAvailable (Boolean available, Pageable pageable);
+
+    Page<Callback> findAll(Pageable pageable);
+
+    Integer countAllByAvailable(Boolean available);
 }

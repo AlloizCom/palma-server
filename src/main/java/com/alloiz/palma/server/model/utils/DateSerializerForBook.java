c9e8ef1ef51e6cb220dfa330906b25cf1c1d8251
unknown <bohdanrud5180@gmail.com>
2018-09-24 17:08:43 +0300

    added serializer/deserializer with time

---
+++ src/main/java/com/alloiz/palma/server/model/utils/DateSerializerForBook.java
@@ -0,0 +1,19 @@
+package com.alloiz.palma.server.model.utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+
+public class DateSerializerForBook extends JsonSerializer<Timestamp> {
+
+    @Override
+    public void serialize(Timestamp timestamp, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeString(timestamp.toLocalDateTime() != null ?
+                timestamp.toLocalDateTime().toLocalDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) : "null");
+    }
+}

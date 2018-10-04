d5831feae06e78cccf1f1cbdd8d471b7f5153adc
unknown <bohdanrud5180@gmail.com>
2018-10-04 15:01:43 +0300

    added new RoomTypes

--- src/main/java/com/alloiz/palma/server/model/enums/RoomType.java
+++ src/main/java/com/alloiz/palma/server/model/enums/RoomType.java
@@ -2,7 +2,9 @@ package com.alloiz.palma.server.model.enums;
 
 public enum RoomType {
 
-    STANDARD,
+    STANDARD_TWO_PLACE,
+    STANDARD_THREE_PLACE,
+    STANDARD_FIVE_PLACE,
     SUPERIOR,
     DELUXE,
     STANDARD_IMPROVED,

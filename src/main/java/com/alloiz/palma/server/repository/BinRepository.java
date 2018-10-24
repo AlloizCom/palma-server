f2f35ee2ce12485df6cdb70c79fcff76fd3eb5e7
bohdanrud <bohdanrud5180@gmail.com>
2018-10-24 11:58:06 +0300

    added repository for Bin

---
+++ src/main/java/com/alloiz/palma/server/repository/BinRepository.java
@@ -0,0 +1,16 @@
+package com.alloiz.palma.server.repository;
+
+import com.alloiz.palma.server.model.Bin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BinRepository extends JpaRepository<Bin, Long> {
+
+    List<Bin> findAllByAvailable(Boolean available);
+
+    Bin findByAvailableAndId(Boolean available, Long id);
+
+}

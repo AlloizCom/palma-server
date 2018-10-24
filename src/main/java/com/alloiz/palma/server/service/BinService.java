fd5e9ee1e481336c4ed31ee9fb4ca54a25cd5575
bohdanrud <bohdanrud5180@gmail.com>
2018-10-24 12:00:42 +0300

    added service for Bin

---
+++ src/main/java/com/alloiz/palma/server/service/BinService.java
@@ -0,0 +1,24 @@
+package com.alloiz.palma.server.service;
+
+import com.alloiz.palma.server.model.Bin;
+import com.alloiz.palma.server.model.enums.Language;
+
+import java.util.List;
+
+public interface BinService {
+
+    Bin findOneAvailable(Long id);
+
+    List<Bin> findAllAvailable();
+
+    Bin findOne(Long id);
+
+    List<Bin> findAll();
+
+    Bin save(Bin bin, Language language);
+
+    Bin update(Bin bin);
+
+    Boolean delete(Long id);
+
+}

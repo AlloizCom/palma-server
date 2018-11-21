3698edd1b844d0b9c46d396c92ad9edfce968b80
Oleh Petryk <oleh.petryk@gmail.com>
2018-11-21 13:24:43 +0200

    fixing cors

--- src/main/java/com/alloiz/palma/server/config/RestConfig.java
+++ src/main/java/com/alloiz/palma/server/config/RestConfig.java
@@ -26,7 +26,7 @@ public class RestConfig implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin")+", 185.233.116.57, http://185.233.116.57");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization,Content-Type,Origin,enctype");

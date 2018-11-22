a8348223f531bb8ba3b7bd057ae49250787fa60c
bohdanrud <bohdanrud5180@gmail.com>
2018-11-22 17:17:55 +0200

    added LanguageService

--- src/main/java/com/alloiz/palma/server/controller/LanguageController.java
+++ src/main/java/com/alloiz/palma/server/controller/LanguageController.java
@@ -1,6 +1,5 @@
 package com.alloiz.palma.server.controller;
 
-import com.alloiz.palma.server.model.enums.Language;
 import com.alloiz.palma.server.service.LanguageService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/language")

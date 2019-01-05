3142754497e2291f482d7b0a3d404af88f6384a3
Volodymyr Bilevych <bilevych85@gmail.com>
2019-01-05 13:46:45 +0200

    fix news update (SEO)

--- src/main/java/com/alloiz/palma/server/service/impl/NewsServiceImpl.java
+++ src/main/java/com/alloiz/palma/server/service/impl/NewsServiceImpl.java
@@ -1,14 +1,20 @@
 package com.alloiz.palma.server.service.impl;
 
+import static com.alloiz.palma.server.config.mapper.JsonMapper.json;
+import static com.alloiz.palma.server.dto.utils.builder.Builder.map;
+import static com.alloiz.palma.server.service.utils.Validation.checkId;
+import static com.alloiz.palma.server.service.utils.Validation.checkJson;
+import static com.alloiz.palma.server.service.utils.Validation.checkObjectExistsById;
+import static com.alloiz.palma.server.service.utils.Validation.checkSave;
+import static java.util.stream.Collectors.toList;
+
 import com.alloiz.palma.server.dto.NewsByPages;
 import com.alloiz.palma.server.dto.NewsFullDto;
-import com.alloiz.palma.server.dto.NewsShortDto;
 import com.alloiz.palma.server.model.News;
 import com.alloiz.palma.server.model.SEO;
 import com.alloiz.palma.server.repository.NewsRepository;
 import com.alloiz.palma.server.service.NewsService;
 import com.alloiz.palma.server.service.utils.FileBuilder;
-import com.google.common.collect.Lists;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -18,170 +24,195 @@ import org.springframework.web.multipart.MultipartFile;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.alloiz.palma.server.config.mapper.JsonMapper.json;
-import static com.alloiz.palma.server.dto.utils.builder.Builder.map;
-import static com.alloiz.palma.server.service.utils.Validation.*;
-import static java.util.stream.Collectors.toList;
 
 
 @Service
-public class NewsServiceImpl implements NewsService {
+public class NewsServiceImpl implements NewsService
+{
 
-    private static final Logger LOGGER = Logger.getLogger(NewsServiceImpl.class);
-    @Autowired
-    private NewsRepository newsRepository;
-    @Autowired
-    private FileBuilder fileBuilder;
+	private static final Logger LOGGER = Logger.getLogger(NewsServiceImpl.class);
+	@Autowired
+	private NewsRepository newsRepository;
+	@Autowired
+	private FileBuilder fileBuilder;
 
-    @Override
-    public News findOneAvailable(Long id) {
-        checkId(id);
-        return newsRepository.findByAvailableAndId(true, id);
-    }
+	@Override
+	public News findOneAvailable(Long id)
+	{
+		checkId(id);
+		return newsRepository.findByAvailableAndId(true, id);
+	}
 
-    @Override
-    public List<News> findAllAvailable() {
-        return newsRepository.findAllByAvailable(true);
-    }
+	@Override
+	public List<News> findAllAvailable()
+	{
+		return newsRepository.findAllByAvailable(true);
+	}
 
-    @Override
-    public News findOne(Long id) {
-        checkId(id);
-        return newsRepository.findOne(id);
-    }
+	@Override
+	public News findOne(Long id)
+	{
+		checkId(id);
+		return newsRepository.findOne(id);
+	}
 
-    @Override
-    public List<News> findAll() {
-        return newsRepository.findAll();
-    }
+	@Override
+	public List<News> findAll()
+	{
+		return newsRepository.findAll();
+	}
 
-    @Override
-    public News save(News news) {
-        checkSave(news);
-        List<SEO> seos = news.getSeos();
-        news = newsRepository.save(news
-                .setDateTime(Timestamp.valueOf(LocalDateTime.now()))
-                .setAvailable(true));
-        News finalNews = news;
-        return newsRepository.save(news.setSeos(seos.stream().map(seo -> seo.setNews(finalNews)).collect(toList())));
-    }
+	@Override
+	public News save(News news)
+	{
+		checkSave(news);
+		List<SEO> seos = news.getSeos();
+		news = newsRepository.save(news
+				.setDateTime(Timestamp.valueOf(LocalDateTime.now()))
+				.setAvailable(true));
+		News finalNews = news;
+		return newsRepository.save(news.setSeos(seos.stream().map(seo -> seo.setNews(finalNews)).collect(toList())));
+	}
 
-    @Override
-    public News save(String newsJson, MultipartFile multipartFile) {
-        checkJson(newsJson);
-        News news = json(newsJson, News.class);
-        news.getNewsDescriptions()
-                .stream()
-                .forEach(newsDescription -> newsDescription.setAvailable(true));
-        if (multipartFile != null && !multipartFile.isEmpty()) {
-            news.setPicturePath(fileBuilder.saveFile(multipartFile));
-        }
-        return save(news);
-    }
+	@Override
+	public News save(String newsJson, MultipartFile multipartFile)
+	{
+		checkJson(newsJson);
+		News news = json(newsJson, News.class);
+		news.getNewsDescriptions()
+				.stream()
+				.forEach(newsDescription -> newsDescription.setAvailable(true));
+		if (multipartFile != null && !multipartFile.isEmpty())
+		{
+			news.setPicturePath(fileBuilder.saveFile(multipartFile));
+		}
+		return save(news);
+	}
 
-    @Override
-    public News update(News news) {
-        checkObjectExistsById(news.getId(), newsRepository);
-        return newsRepository.save(findOne(news.getId())
-                .setNewsDescriptions(news.getNewsDescriptions())
-                .setAvailable(news.getAvailable())
-                .setPicturePath(news.getPicturePath()));
-    }
+	@Override
+	public News update(News news)
+	{
+		checkObjectExistsById(news.getId(), newsRepository);
+		return newsRepository.save(findOne(news.getId())
+				.setNewsDescriptions(news.getNewsDescriptions())
+				.setAvailable(news.getAvailable())
+				.setPicturePath(news.getPicturePath())
+				.setSeos(news.getSeos())
+		);
+	}
 
-    @Override
-    public News update(String newsJson, MultipartFile multipartFile) {
-        checkJson(newsJson);
-        News news = json(newsJson, News.class);
-        if (multipartFile != null && !multipartFile.isEmpty()) {
-            news.setPicturePath(fileBuilder.saveFile(multipartFile));
-        }
-        return update(news);
-    }
+	@Override
+	public News update(String newsJson, MultipartFile multipartFile)
+	{
+		checkJson(newsJson);
+		News news = json(newsJson, News.class);
+		if (multipartFile != null && !multipartFile.isEmpty())
+		{
+			news.setPicturePath(fileBuilder.saveFile(multipartFile));
+		}
+		return update(news);
+	}
 
-    @Override
-    public Boolean delete(Long id) {
-        LOGGER.info("---------------------------News---------------------------");
-        LOGGER.info("--------------------------DELETE--------------------------");
-        LOGGER.info("--------------------------SERVICE-------------------------");
-        LOGGER.info("News ID:" + id);
-        LOGGER.info("---------------------------News---------------------------");
-        try {
-            newsRepository.delete(checkObjectExistsById(id, newsRepository));
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
+	@Override
+	public Boolean delete(Long id)
+	{
+		LOGGER.info("---------------------------News---------------------------");
+		LOGGER.info("--------------------------DELETE--------------------------");
+		LOGGER.info("--------------------------SERVICE-------------------------");
+		LOGGER.info("News ID:" + id);
+		LOGGER.info("---------------------------News---------------------------");
+		try
+		{
+			newsRepository.delete(checkObjectExistsById(id, newsRepository));
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
 
-    @Override
-    public List<Integer> generateRandomArray(int length) {
-        List<Integer> list = getRandomArray(length);
-        return list;
-    }
-
-
-    @Override
-    public List<News> findRandomNews(int amount) {
-        if (newsRepository.findAllByAvailable(true).size() >= 6) {
-            List<News> randomNews;
-            randomNews = newsRepository.findAllByAvailable(true);
-            Collections.shuffle(randomNews);
-            return randomNews.subList(0, amount);
-        } else {
-            return newsRepository.findAllByAvailable(true);
-        }
-    }
+	@Override
+	public List<Integer> generateRandomArray(int length)
+	{
+		List<Integer> list = getRandomArray(length);
+		return list;
+	}
 
 
-    private List<Integer> getRandomArray(int length) {
-        List<Integer> list = new ArrayList<>();
-        int maxIndex = newsRepository.findAllByAvailable(true).size();
-        if (maxIndex >= length) {
-            for (int i = 0; i < length; i++) {
-                while (true) {
-                    Integer temp = (int) Math.floor(Math.random() * maxIndex);
-                    if (list.indexOf(temp) == -1) {
-                        list.add(temp);
-                        break;
-                    }
-                }
-            }
-        } else
-            list.stream().forEach(elem -> elem = maxIndex);
+	@Override
+	public List<News> findRandomNews(int amount)
+	{
+		if (newsRepository.findAllByAvailable(true).size() >= 6)
+		{
+			List<News> randomNews;
+			randomNews = newsRepository.findAllByAvailable(true);
+			Collections.shuffle(randomNews);
+			return randomNews.subList(0, amount);
+		}
+		else
+		{
+			return newsRepository.findAllByAvailable(true);
+		}
+	}
 
-        LOGGER.info("-----------ARRAY-------" + list);
-        return list;
-    }
 
-    @Override
-    public List<News> findAll(Pageable pageable) {
-        List<News> newsList = newsRepository.findAll(pageable).getContent();
-        return newsList;
-    }
+	private List<Integer> getRandomArray(int length)
+	{
+		List<Integer> list = new ArrayList<>();
+		int maxIndex = newsRepository.findAllByAvailable(true).size();
+		if (maxIndex >= length)
+		{
+			for (int i = 0; i < length; i++)
+			{
+				while (true)
+				{
+					Integer temp = (int) Math.floor(Math.random() * maxIndex);
+					if (list.indexOf(temp) == -1)
+					{
+						list.add(temp);
+						break;
+					}
+				}
+			}
+		}
+		else
+		{
+			list.stream().forEach(elem -> elem = maxIndex);
+		}
 
-    @Override
-    public NewsByPages findAllByAvailable(Pageable pageable) {
-        LOGGER.info(">>>" + pageable.getPageNumber());
-        LOGGER.info(">>>" + pageable.getPageSize());
-        List<NewsFullDto> newsList = newsRepository.findAllByAvailableOrderByDateTimeDesc(true, pageable)
-                .getContent()
-                .stream()
-                .map(news -> map(news, NewsFullDto.class))
-                .collect(toList());
-        LOGGER.info("-------------News Page---------------");
-        newsList.stream().forEach(n -> LOGGER.info(n.getId()));
-        LOGGER.info("-----------------------------------");
-        newsList.stream().forEach(n -> LOGGER.info(n.getId()));
-        return new NewsByPages()
-                .setNews(newsList)
-                .setCurrentPage(pageable.getPageNumber())
-                .setNumberOfItems(pageable.getPageSize())
-                .setNumberOfPages((newsRepository.countAllByAvailable(true) / pageable.getPageSize()) + 1);
+		LOGGER.info("-----------ARRAY-------" + list);
+		return list;
+	}
 
-    }
+	@Override
+	public List<News> findAll(Pageable pageable)
+	{
+		List<News> newsList = newsRepository.findAll(pageable).getContent();
+		return newsList;
+	}
+
+	@Override
+	public NewsByPages findAllByAvailable(Pageable pageable)
+	{
+		LOGGER.info(">>>" + pageable.getPageNumber());
+		LOGGER.info(">>>" + pageable.getPageSize());
+		List<NewsFullDto> newsList = newsRepository.findAllByAvailableOrderByDateTimeDesc(true, pageable)
+				.getContent()
+				.stream()
+				.map(news -> map(news, NewsFullDto.class))
+				.collect(toList());
+		LOGGER.info("-------------News Page---------------");
+		newsList.stream().forEach(n -> LOGGER.info(n.getId()));
+		LOGGER.info("-----------------------------------");
+		newsList.stream().forEach(n -> LOGGER.info(n.getId()));
+		return new NewsByPages()
+				.setNews(newsList)
+				.setCurrentPage(pageable.getPageNumber())
+				.setNumberOfItems(pageable.getPageSize())
+				.setNumberOfPages((newsRepository.countAllByAvailable(true) / pageable.getPageSize()) + 1);
+
+	}
 }

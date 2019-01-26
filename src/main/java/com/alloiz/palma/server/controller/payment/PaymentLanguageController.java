2e17a09d86d904c97b6dece38c08f48a2ea40fa2
Volodymyr Bilevych <bilevych85@gmail.com>
2019-01-26 15:00:27 +0200

    fix controller beans names conflict

--- src/main/java/com/alloiz/palma/server/controller/payment/PaymentLanguageController.java
+++ src/main/java/com/alloiz/palma/server/controller/payment/PaymentLanguageController.java
@@ -13,13 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-@RestController("/payment_language")
+@RestController
+@RequestMapping("/payment_language")
 public class PaymentLanguageController
 {
 
